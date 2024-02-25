d3a534aa3ef406a19916a1bd8f7755744e34d2cc
EditaTumanyan <edita.tumanyan9898@gmail.com>
2024-02-25 15:33:10 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./assets/components/Header"
+import Todo from "./assets/components/todo"
+import "./App.css"
+
+function App() {
+  
+  return (
+ <div>
+   <Header />
+   <Todo />
+ </div>
+  )
+}
+
+export default App
